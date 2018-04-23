bfc787f7a6e8f4fe645e32c0cd1bc66234b78df6
Jérémy Seban <jseban@francechain.fr>
2018-04-23 22:06:51 +0200

    Updated contract to thesaurio-solidity 0.0.8

--- contracts/Sample.sol
+++ contracts/Sample.sol
@@ -14,9 +14,11 @@ import "thesaurio-solidity/contracts/lib/MoneyOperations.sol";
  * It is meant to be used in a crowdsale contract.
  */
 contract SampleCrowdsaleToken is MintableToken {
-  string public constant name = "Sample Crowdsale Token"; // solium-disable-line uppercase
-  string public constant symbol = "SCT"; // solium-disable-line uppercase
-  uint8 public constant decimals = 18; // solium-disable-line uppercase
+
+    string public constant name = "Sample Crowdsale Token"; // solium-disable-line uppercase
+    string public constant symbol = "SCT"; // solium-disable-line uppercase
+    uint8 public constant decimals = 18; // solium-disable-line uppercase
+
 }
 
 /**
@@ -35,41 +37,41 @@ contract SampleCrowdsaleToken is MintableToken {
  * to ensure that subcontracts works together as intended.
  */
 contract SampleCrowdsale is MintedCrowdsale, FiatCompatible, KycCompatible, ThesaurioInfoCompatible {
-  using MoneyOperations for uint256;
+    using MoneyOperations for uint256;
 
-  uint256 constant minContrib = 0.1 ether;
-  uint256 constant maxContrib = 100 ether;
+    uint256 constant minContrib = 0.1 ether;
+    uint256 constant maxContrib = 100 ether;
 
-  constructor(
-    uint256 _rate,
-    address _wallet,
-    MintableToken _token,
-    address _priceRegistry,
-    address _kycRegistry
-  ) public
-    Crowdsale(_rate, _wallet, _token)
-    FiatCompatible(_priceRegistry)
-    KycCompatible(_kycRegistry) {}
+    constructor(
+        uint256 _rate,
+        address _wallet,
+        MintableToken _token,
+        address _priceRegistry,
+        address _kycRegistry
+    ) public
+        Crowdsale(_rate, _wallet, _token)
+        FiatCompatible(_priceRegistry)
+        KycCompatible(_kycRegistry) {}
 
-  function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
-    require(_beneficiary != address(0));
-    require(_weiAmount != 0 && _weiAmount >= minContrib && _weiAmount <= maxContrib);
-    require(kycRegistry.isAddressCleared(_beneficiary));
-  }
+    function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
+        require(_beneficiary != address(0));
+        require(_weiAmount != 0 && _weiAmount >= minContrib && _weiAmount <= maxContrib);
+        require(kycRegistry.isAddressCleared(_beneficiary));
+    }
 
-  function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
-    return _weiAmount.ethdiv(fiatPrices.eurPrice().mul(rate));
-  }
+    function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
+        return _weiAmount.ethdiv(fiatPrices.eurPrice().mul(rate));
+    }
 
-  function distributionInfo() public view returns (
-    uint256 minimumContribution,
-    uint256 maximumContribution,
-    uint256 currentTokenPrice,
-    uint256 remainingSupply
-  ) {
-    minimumContribution = minContrib;
-    maximumContribution = maxContrib;
-    currentTokenPrice = fiatPrices.eurPrice().mul(rate);
-    remainingSupply = 0; // Ignored parameter, no cap
-  }
+    function distributionInfo() public view returns (
+        uint256 minimumContribution,
+        uint256 maximumContribution,
+        uint256 currentTokenPrice,
+        uint256 remainingSupply
+    ) {
+        minimumContribution = minContrib;
+        maximumContribution = maxContrib;
+        currentTokenPrice = fiatPrices.eurPrice().mul(rate);
+        remainingSupply = 0; // Ignored parameter, no cap
+    }
 }
